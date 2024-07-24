debae9bb80d1107222fd41639aeada855b48969f
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-07-24 21:03:20 +0300

    Returning an empty promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const n = true;
-    if (n) {
-      resolve("Yey! it's true!");
-    } else {
-      reject(Error('It failed :('));
-    }
-  });
+  return new Promise(() => {});
 }
