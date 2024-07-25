432b8447800fe262c0a7d03d83fda5920c171d84
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-07-25 23:29:04 +0300

    Uptated validators in 4-pricing.js, created 5-building.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -4,7 +4,10 @@ export default class Pricing {
   constructor(amount, currency) {
     this.amount = amount;
     this.currency = currency;
-    this.displayFullPrice = () => `${amount} ${currency.name} ${currency.code}`;
+  }
+
+  displayFullPrice() {
+    return `${amount} ${currency.name} ${currency.code}`;
   }
 
   get amount() {
@@ -16,14 +19,26 @@ export default class Pricing {
   }
 
   set amount(x) {
+    if (typeof x !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
     this._amount = x;
   }
 
   set currency(x) {
+    if (!(x instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
     this._currency = x;
   }
 
-  convertPrice(amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
     return amount * conversionRate;
   }
 }
